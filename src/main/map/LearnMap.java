39dc8bf0f03521f7a35f9c5418b83b0690b63af3
dsz <364615401@qq.com>
2021-11-05 22:16:19 +0800

    java源码解析

---
+++ src/main/map/LearnMap.java
@@ -0,0 +1,10 @@
+package main.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LearnMap {
+    public static void main(String[] args) {
+        Map map=new HashMap();
+    }
+}
